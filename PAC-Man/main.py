2d7ca93c276c0c28bca5045dad0ccbc2ca2ccd4c
vharipal <91626199+vharipal@users.noreply.github.com>
2024-03-05 00:42:19 -0500

    Add files via upload

--- PAC-Man/main.py
+++ PAC-Man/main.py
@@ -18,32 +18,43 @@ def login():
     return render_template('login.html')
 
 @app.route('/newvault', methods=['POST'])
- #To Do: Add case for if user already exists in Database
+#To Do: Add case for if user already exists in Database
+#To Do: Verify valid email
 def newvault():
-    email = request.form["email"]
-    password = request.form["masterpass"]
+    email = request.form["email"].lower()
+    password1 = request.form["masterpass1"]
+    password2 = request.form["masterpass2"]
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO pacusers (email, masterpass) VALUES (%s, %s);", (email, password))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return render_template("vault.html")
+    #check if user correctly entered password both times
+    if  password1 == password2:
+        cur.execute("INSERT INTO pacusers (email, masterpass) VALUES (%s, %s);", (email, password2))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return render_template("vault.html")
+    else: 
+        cur.close()
+        conn.close()
+        return render_template("errorsignup.html")
 
 @app.route('/vault', methods=['POST'])
 def vault():
-    email = request.form["email"]
+    email = request.form["email"].lower()
     password = request.form["masterpass"]
     conn = get_db_connection()
     cur = conn.cursor()
+    #search databse for entry matching email/pass credential
     cur.execute("SELECT * FROM pacusers WHERE email = %s AND masterpass = %s;", (email, password))
     valid = cur.fetchall()
     cur.close()
     conn.close()
+    #if SQL Query finds a math valid = true
     if valid:
-        return render_template('vault.html')
+        return render_template("vault.html")
     else:
-       return("User not Found")
+       return render_template("errorlogin.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #turn off debug when running with host = 0.0.0.0
+    app.run(debug=True, ssl_context='adhoc', host='0.0.0.0')
